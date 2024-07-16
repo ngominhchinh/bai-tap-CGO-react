68ce4e46b842abd30e0df093b2733d46732bff52
ngominhchinh <ngominhchinhdhhd5@gmail.com>
2024-07-16 18:36:02 +0700

    browser

--- src/index.js
+++ src/index.js
@@ -1,17 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const name = "Your name";
+React.createElement("h1", {style:{textAlign: "center"}},name);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  React.createElement("h1", { style: { textAlign: "center" } }, name)
 );
+//============================================================================
+// import ReactDOM from 'react-dom';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// const fruits = ['Apple', 'Banana', 'Orange', 'Apricot', 'Black rowan', 'Cranberry']
+
+// ReactDOM.render(
+// <div>
+//     <h1>List of fruits</h1>
+//     <ul>
+//       { fruits.map((item) => (
+//         <li>{ item }</li>
+//       )) }
+//     </ul>
+//  </div>,
+// document.getElementById(‘root’)
+// );
+
+//================================================================
+// import ReactDOM from "react-dom/client";
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// const tick = () => {
+//   root.render(
+//     <div>
+//       <h1>Hello, world!</h1>
+//       <h2>It is {new Date().toLocaleTimeString()}.</h2>
+//     </div>
+//   );
+// };
+
+// setInterval(tick, 1000);
+
+//============================================================================
+// import ReactDOM from 'react-dom';
+
+// const fruits = ['Apple', 'Banana', 'Orange', 'Apricot', 'Black rowan', 'Cranberry']
+
+// ReactDOM.render(
+// <div>
+//     <h4>Browser's details: {navigator.userAgent}</h4>
+//     
+// document.getElementById(‘root’)
+// );
